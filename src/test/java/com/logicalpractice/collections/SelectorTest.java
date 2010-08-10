20e084cf63e50abbc057f5b23b1ab02390873538
Gareth Davis <gareth@logicalpractice.com>
2010-08-10 07:41:35 +0100

    reformating

--- src/test/java/com/logicalpractice/collections/SelectorTest.java
+++ src/test/java/com/logicalpractice/collections/SelectorTest.java
@@ -34,144 +34,144 @@ import com.logicalpractice.collections.support.MethodCapture;
 
 public class SelectorTest {
 
-   List<Person> testData = Arrays.asList(new Person("Billy", "Smith", 8), new Person("Billy",
-         "Jones", 32), new Person("James", "Smith", 70));
+    List<Person> testData = Arrays.asList(new Person("Billy", "Smith", 8), new Person("Billy",
+            "Jones", 32), new Person("James", "Smith", 70));
 
-   @Test
-   public void selectAllSmiths() throws Exception {
+    @Test
+    public void selectAllSmiths() throws Exception {
 
-      List<Person> output = newArrayList(select(testData, new Expression<Person,String>() {
-         {
-            each(Person.class).getLastName();
-         }
-      }, equalTo("Smith")));
+        List<Person> output = newArrayList(select(testData, new Expression<Person, String>() {
+            {
+                each(Person.class).getLastName();
+            }
+        }, equalTo("Smith")));
 
-      assertThat(output.size(), equalTo(2));
-   }
+        assertThat(output.size(), equalTo(2));
+    }
 
-   @Test
-   public void selectAllSmiths2() throws Exception {
-      List<Person> output = newArrayList(select(testData, where(Person.class).getLastName(), equalTo("Smith")));
-      assertThat(output.size(), equalTo(2));
-   }
+    @Test
+    public void selectAllSmiths2() throws Exception {
+        List<Person> output = newArrayList(select(testData, where(Person.class).getLastName(), equalTo("Smith")));
+        assertThat(output.size(), equalTo(2));
+    }
 
-   @Test
-   public void selectAllSmiths3() throws Exception {
-      Iterable<Person> output = select(from(testData).getLastName(), equalTo("Smith"));
-      assertThat(size(output), equalTo(2));
-   }
+    @Test
+    public void selectAllSmiths3() throws Exception {
+        Iterable<Person> output = select(from(testData).getLastName(), equalTo("Smith"));
+        assertThat(size(output), equalTo(2));
+    }
 
-   @Test
-   public void selectAllBillys() throws Exception {
-      Iterable<Person> output = select(from(testData).getFirstName(), equalToIgnoringCase("billy"));
-      assertThat(size(output), equalTo(2));
-   }
+    @Test
+    public void selectAllBillys() throws Exception {
+        Iterable<Person> output = select(from(testData).getFirstName(), equalToIgnoringCase("billy"));
+        assertThat(size(output), equalTo(2));
+    }
 
-   @Test(expected = IllegalArgumentException.class)
-   public void fromThrowsIllegalArgumentException() throws Exception {
-      List<String> values = Collections.emptyList();
-      from(values);
-   }
+    @Test(expected = IllegalArgumentException.class)
+    public void fromThrowsIllegalArgumentException() throws Exception {
+        List<String> values = Collections.emptyList();
+        from(values);
+    }
 
-   @Test
-   public void selectAllowsTypedZeroLengthInput() throws Exception {
-      List<Person> values = Collections.emptyList();
-      values = typedList(values, Person.class);
-      Iterable<Person> results = select(from(values).getFirstName(), equalTo("Thing"));
+    @Test
+    public void selectAllowsTypedZeroLengthInput() throws Exception {
+        List<Person> values = Collections.emptyList();
+        values = typedList(values, Person.class);
+        Iterable<Person> results = select(from(values).getFirstName(), equalTo("Thing"));
 
-      assertThat(size(results), equalTo(0));
-   }
+        assertThat(size(results), equalTo(0));
+    }
 
-   @Test
-   public void selectUsingPredicate() {
-      final AtomicInteger evaluateCount = new AtomicInteger(0);
+    @Test
+    public void selectUsingPredicate() {
+        final AtomicInteger evaluateCount = new AtomicInteger(0);
 
-      Iterable<Person> result = select(testData, new Predicate<Person>() {
-         public boolean apply(Person p) {
-            evaluateCount.incrementAndGet();
-            return p.getLastName().matches("[Ss]mith");
-         }
-      });
-      assertThat(size(result), equalTo(2));
-      assertThat(evaluateCount.intValue(), equalTo(3));
-   }
+        Iterable<Person> result = select(testData, new Predicate<Person>() {
+            public boolean apply(Person p) {
+                evaluateCount.incrementAndGet();
+                return p.getLastName().matches("[Ss]mith");
+            }
+        });
+        assertThat(size(result), equalTo(2));
+        assertThat(evaluateCount.intValue(), equalTo(3));
+    }
 
-   @Test
-   public void simpleSelect() throws Exception {
-      Integer[] values = { 1, 3, 7, 9, 11, 19 };
+    @Test
+    public void simpleSelect() throws Exception {
+        Integer[] values = {1, 3, 7, 9, 11, 19};
 
-      Iterable<Integer> result = select(asList(values), lessThan(10));
+        Iterable<Integer> result = select(asList(values), lessThan(10));
 
-      assertThat(result, not(nullValue()));
-      assertThat(result, hasItems(1, 3, 7, 9));
-      assertThat(result, not(hasItems(11, 19)));
-   }
+        assertThat(result, not(nullValue()));
+        assertThat(result, hasItems(1, 3, 7, 9));
+        assertThat(result, not(hasItems(11, 19)));
+    }
 
-   @Test
-   public void scriptCollectOperation() {
+    @Test
+    public void scriptCollectOperation() {
 
-      // collects all the first names
-       Iterable<String> result = collect(testData, new Expression<Person,String>() {{
-           each(Person.class).getFirstName();
-       }});
+        // collects all the first names
+        Iterable<String> result = collect(testData, new Expression<Person, String>() {{
+            each(Person.class).getFirstName();
+        }});
 
-       assertThat(result, not(nullValue()));
-      assertThat(result, hasItems("Billy", "James"));
-      assertThat(size(result), equalTo(3));
-   }
+        assertThat(result, not(nullValue()));
+        assertThat(result, hasItems("Billy", "James"));
+        assertThat(size(result), equalTo(3));
+    }
 
-   @Test
-   public void scriptCollectOperation2() {
+    @Test
+    public void scriptCollectOperation2() {
 
-      // collects all the first names
-      Iterable<String> result = collect(testData, new Expression<Person,String>() {
-         {
-            each(Person.class).getLastName();
-         }
-      });
+        // collects all the first names
+        Iterable<String> result = collect(testData, new Expression<Person, String>() {
+            {
+                each(Person.class).getLastName();
+            }
+        });
 
-      assertThat(result, not(nullValue()));
-      assertThat(result, hasItems("Smith", "Jones"));
-      assertThat(size(result), equalTo(3));
-   }
+        assertThat(result, not(nullValue()));
+        assertThat(result, hasItems("Smith", "Jones"));
+        assertThat(size(result), equalTo(3));
+    }
 
-   @Test
-   public void byCollectOperation() {
+    @Test
+    public void byCollectOperation() {
 
-      Iterable<String> result = collect(testData, by(Person.class).getLastName());
+        Iterable<String> result = collect(testData, by(Person.class).getLastName());
 
-      assertThat(result, not(nullValue()));
-      assertThat(result, hasItems("Smith", "Jones"));
-      assertThat(size(result), equalTo(3));
-   }
+        assertThat(result, not(nullValue()));
+        assertThat(result, hasItems("Smith", "Jones"));
+        assertThat(size(result), equalTo(3));
+    }
 
-   @SuppressWarnings("unchecked")
-   @Test
-   public void checkPrimitiveMethod() throws Exception {
+    @SuppressWarnings("unchecked")
+    @Test
+    public void checkPrimitiveMethod() throws Exception {
 
-      Iterable<Person> workingAgeAdults = select(from(testData).getAge(), allOf(greaterThan(18),
-            lessThan(65)));
+        Iterable<Person> workingAgeAdults = select(from(testData).getAge(), allOf(greaterThan(18),
+                lessThan(65)));
 
-      assertThat(size(workingAgeAdults), greaterThan(0));
-   }
+        assertThat(size(workingAgeAdults), greaterThan(0));
+    }
 
-   @Test(expected = NullPointerException.class)
-   public void verifyNullPointerThrown() throws Exception {
-      select(testData, where(Person.class).getFirstName()
-            .substring(0, 1), equalTo("s"));
-   }
+    @Test(expected = NullPointerException.class)
+    public void verifyNullPointerThrown() throws Exception {
+        select(testData, where(Person.class).getFirstName()
+                .substring(0, 1), equalTo("s"));
+    }
 
-   @Test
-   public void collectFromOperation() {
-      Iterable<String> result = collect(from(testData).getLastName());
+    @Test
+    public void collectFromOperation() {
+        Iterable<String> result = collect(from(testData).getLastName());
 
-      assertThat(result, not(nullValue()));
-      assertThat(result, hasItems("Smith", "Jones"));
-      assertThat(size(result), equalTo(3));
-   }
+        assertThat(result, not(nullValue()));
+        assertThat(result, hasItems("Smith", "Jones"));
+        assertThat(size(result), equalTo(3));
+    }
 
-   @After
-   public void clearAndReturn() {
-      MethodCapture.clearAndReturn();
-   }
+    @After
+    public void clearAndReturn() {
+        MethodCapture.clearAndReturn();
+    }
 }
